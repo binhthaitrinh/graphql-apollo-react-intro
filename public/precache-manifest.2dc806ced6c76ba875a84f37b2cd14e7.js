24c5f95a7b2f6dd06d144700bcbebb42c6da1a2b
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-08-12 23:59:52 -0700

    Ready for deplyment

---
+++ public/precache-manifest.2dc806ced6c76ba875a84f37b2cd14e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3afd14c3e84a38e3a4f54cb396dcb08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddff9d4eaae5bdc84324",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "6e569f46ac9a16398c47",
+    "url": "/static/js/2.8a642900.chunk.js"
+  },
+  {
+    "revision": "ddff9d4eaae5bdc84324",
+    "url": "/static/js/main.837e690b.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
